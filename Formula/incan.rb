95e0f2e5552cd18b348365a4d1a377530eb9a6d3
Danny Meijer <chilltake@gmail.com>
2026-07-07 21:46:54 +0200

    formula - satisfy homebrew audit for incan 0.4.0

--- Formula/incan.rb
+++ Formula/incan.rb
@@ -4,21 +4,16 @@
 class Incan < Formula
   desc "Statically typed language that compiles to native Rust"
   homepage "https://github.com/encero-systems/incan"
-  version "0.4.0"
 
-  on_macos do
-    if Hardware::CPU.arm?
-      url "https://github.com/encero-systems/incan/releases/download/v0.4.0/incan-v0.4.0-aarch64-apple-darwin.tar.gz"
-      sha256 "f1cc83611de33808b609f814c9cec7fe59c0e3315e2767f65ecda9c71e9b966a"
-    else
-      url "https://github.com/encero-systems/incan/releases/download/v0.4.0/incan-v0.4.0-x86_64-apple-darwin.tar.gz"
-      sha256 "f5f4b13c85b0823b1d517a3ac618441e7147eb9987f1f3450bcefbe26fabca70"
-    end
-  end
-
-  on_linux do
-      url "https://github.com/encero-systems/incan/releases/download/v0.4.0/incan-v0.4.0-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "f39e941e3fc0c817de9656b78b1c8899ef226784f095f9fff02efac3f06562b0"
+  if OS.mac? && Hardware::CPU.arm?
+    url "https://github.com/encero-systems/incan/releases/download/v0.4.0/incan-v0.4.0-aarch64-apple-darwin.tar.gz"
+    sha256 "f1cc83611de33808b609f814c9cec7fe59c0e3315e2767f65ecda9c71e9b966a"
+  elsif OS.mac?
+    url "https://github.com/encero-systems/incan/releases/download/v0.4.0/incan-v0.4.0-x86_64-apple-darwin.tar.gz"
+    sha256 "f5f4b13c85b0823b1d517a3ac618441e7147eb9987f1f3450bcefbe26fabca70"
+  elsif OS.linux? && Hardware::CPU.intel?
+    url "https://github.com/encero-systems/incan/releases/download/v0.4.0/incan-v0.4.0-x86_64-unknown-linux-gnu.tar.gz"
+    sha256 "f39e941e3fc0c817de9656b78b1c8899ef226784f095f9fff02efac3f06562b0"
   end
 
   def staged_files
@@ -42,7 +37,9 @@ class Incan < Formula
     stdlib_dir = Pathname.new("stdlib")
     odie "could not find incan binary in archive; staged files: #{staged_file_sample}" if incan_bin.nil?
     odie "could not find incan-lsp binary in archive; staged files: #{staged_file_sample}" if incan_lsp_bin.nil?
-    odie "could not find stdlib/testing.incn in archive; staged files: #{staged_file_sample}" unless (stdlib_dir/"testing.incn").exist?
+    unless (stdlib_dir/"testing.incn").exist?
+      odie "could not find stdlib/testing.incn in archive; staged files: #{staged_file_sample}"
+    end
     libexec.install Dir["*"]
     bin.write_exec_script libexec/"bin/incan"
     bin.write_exec_script libexec/"bin/incan-lsp"
